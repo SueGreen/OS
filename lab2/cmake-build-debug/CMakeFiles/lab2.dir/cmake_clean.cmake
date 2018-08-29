149f8446ac1fc2a06cdf65d967ef86a5c96765be
Susan Green <35573111+SueGreen@users.noreply.github.com>
2018-08-29 23:58:51 +0300

    Add files via upload

---
+++ lab2/cmake-build-debug/CMakeFiles/lab2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab2.dir/ex1.c.o"
+  "lab2.pdb"
+  "lab2.exe"
+  "liblab2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lab2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
