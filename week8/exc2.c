f7a38847e9ef6e142dde7018796416904ced097d
Susan Green <35573111+SueGreen@users.noreply.github.com>
2018-10-11 22:07:15 +0300

    Update exc2.c

--- week8/exc2.c
+++ week8/exc2.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <zconf.h>
 
+int main(){
+    int bytes = (1024*1024);
+    char* ptr;
+    for (int i = 0; i < 10; i++){
+        ptr = (char*) malloc(10 * bytes);
+        memset(ptr, '0', 10*bytes);
+        printf("%d", i);
+        sleep(1);
+    }
+    return 0;
+}
