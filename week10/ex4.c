f3cf529ee34d1c52a56e3c5e6f48505515778bee
Susan Green <35573111+SueGreen@users.noreply.github.com>
2018-10-25 23:29:17 +0300

    Update ex4.c

--- week10/ex4.c
+++ week10/ex4.c
@@ -1,10 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
-#include <dirent.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <string.h>
+#include <unistd.h>
+#include <dirent.h>
 
 void output_result(char *file, char *dir, ino_t inode);
 
@@ -17,7 +17,7 @@ int main() {
     while ((dp = readdir(dirp)) != NULL) {
         sprintf(file_name, "%s/%s", "./tmp", dp->d_name);
         stat(file_name, &sdata);
-        if (sdata.st_nlink >= 2 && strcmp(dp->d_name, ".") != 0 && strcmp(dp->d_name, "..") != 0) {
+        if (sdata.st_nlink > 1 && strcmp(dp->d_name, ".") != 0 && strcmp(dp->d_name, "..") != 0) {
             output_result(dp->d_name, "./tmp", sdata.st_ino);
         }
     }
@@ -25,12 +25,12 @@ int main() {
 }
 
 void output_result(char *file, char *dir, ino_t inode) {
-    printf("Printing links for file: %s\n", file);
+    printf("File: %s:\n", file);
     DIR *dirp = opendir(dir);
     if (dirp == NULL) return;
-    char file_name[64];
     struct stat sdata;
     struct dirent *dp;
+    char file_name[64];
     while ((dp = readdir(dirp)) != NULL) {
         sprintf(file_name, "%s/%s", dir, dp->d_name);
         stat(file_name, &sdata);
