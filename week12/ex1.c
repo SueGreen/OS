9d3e8043ccf836227860981cb3b49dba519df27f
Susan Green <35573111+SueGreen@users.noreply.github.com>
2018-11-08 22:44:15 +0300

    Update ex1.c

--- week12/ex1.c
+++ week12/ex1.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
 
+#define string_length 20
+
+int main() {
+    unsigned char buffer[string_length];
+    int fd = open("/dev/random", O_RDONLY);
+    if (fd < 0) {
+        return -1;
+    }
+    int sequence = read(fd, buffer, string_length);
+    if (sequence < 0) {
+        return -1;
+    }
+    close(fd);
+    FILE *my_file = fopen("ex1.txt", "w");
+    fprintf(my_file, "%s", buffer);
+    fclose(my_file);
+    return 0;
+}
